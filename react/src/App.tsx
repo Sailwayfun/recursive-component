8bf476252f77287da88d1f772556ed6b11db6d48
Sailwayfun <liaoleon000513@gmail.com>
2024-07-24 09:18:18 +0800

    use recursion

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Folders from "./components/Folders";
 
 function App() {
-
   return (
     <>
       <h1>Recursive Component</h1>
+      <Folders />
     </>
   );
 }
--- react/src/components/Folders/index.tsx
+++ react/src/components/Folders/index.tsx
@@ -1,7 +1,49 @@
 import { FolderIcon } from "@heroicons/react/24/solid";
 
+interface Folder {
+  name: string;
+  folders?: Folder[];
+}
+
+const folders: Folder[] = [
+  {
+    name: "Home",
+    folders: [
+      {
+        name: "Movies",
+        folders: [{ name: "Kimi No Na Wa" }, { name: "Weathering with You" }],
+      },
+      { name: "Music" },
+      { name: "Pictures" },
+    ],
+  },
+];
+
 export default function Folders() {
-    return (
-        <div></div>
-    )
+  return (
+    <div className="p-8 max-w-sm mx-auto">
+      <ul>
+        {folders.map((folder) => (
+          <Folder folder={folder} key={folder.name} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Folder({ folder }) {
+  return (
+    <li className="my-1.5" key={folder.name}>
+      <span className="flex items-center gap-1.5">
+        <FolderIcon className="size-6 text-sky-600" />
+        {folder.name}
+      </span>
+
+      <ul className="pl-6">
+        {folder.folders?.map((folder) => (
+          <Folder folder={folder} key={folder.name} />
+        ))}
+      </ul>
+    </li>
+  );
 }
