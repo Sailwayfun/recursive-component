93045affb93b5dca5be2bf3a43f9a62f7434f161
Sailwayfun <liaoleon000513@gmail.com>
2024-07-24 13:21:17 +0800

    display files

--- vue/src/components/Folders/FileSystemItem.vue
+++ vue/src/components/Folders/FileSystemItem.vue
@@ -1,7 +1,14 @@
 <template>
   <li class="my-1.5" :key="folder!.name">
-    <span className="flex items-center gap-1.5"></span>
-    {{ folder!.name }}
+    <span class="flex items-center gap-1.5">
+      <template v-if="folder!.nodes">
+        <FolderIcon class="size-6 text-sky-600" />
+      </template>
+      <template v-else>
+        <DocumentIcon class="size-6 text-gray-600" />
+      </template>
+      {{ folder!.name }}
+    </span>
     <ul class="pl-6">
       <template v-if="folder!.nodes">
         <FileSystemItem v-for="folder in folder!.nodes" :key="folder.name" :folder="folder" />
@@ -13,6 +20,7 @@
 <script setup lang="ts">
 import { type PropType, toRefs } from 'vue'
 import { type Node } from './FolderList.vue'
+import { FolderIcon, DocumentIcon } from '@heroicons/vue/24/solid'
 
 const props = defineProps({
   folder: { type: Object as PropType<Node> }
