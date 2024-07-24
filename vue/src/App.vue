edcff05b6a2017fb4a213397081d1e8c7a5aa758
Sailwayfun <liaoleon000513@gmail.com>
2024-07-24 12:25:15 +0800

    setup Vue project

---
+++ vue/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Recursive Component</h1>
+    </div>
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
